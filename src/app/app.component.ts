3ad41da1ff6822cf7fc42d66f8f3ea8b7139a624
Tanaka <tanakakenne2000@gmail.com>
2022-03-23 20:55:05 +0200

    now reading all countries

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Country, HttpService } from './services/http.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'rest-countries';
+  countries: Country[] = []
+
+  constructor(http: HttpService) {
+    http.getAll()
+      .subscribe(c => this.countries = c)
+  }
 }
---
+++ src/app/services/http.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor(private http: HttpClient) { 
+    
+  }
+
+  getAll(): Observable<Country[]> {
+    return this.http.get<Country[]>('https://restcountries.com/v3.1/all');
+  }
+}
+
+export interface Country {
+  name: {
+    common: string;
+  };
+  population: number;
+  region: string;
+  capital: string;
+  flags: {
+    png: string;
+    svg?: string;
+  }
+}
